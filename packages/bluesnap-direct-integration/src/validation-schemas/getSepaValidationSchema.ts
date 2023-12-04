2a7da3967c631f3fe95b458b9dfc452822c1b082
Vitaliy Koshovyi <vitaliy.koshovyi@bigcommerce.com>
2023-12-04 19:09:56 +0200

    feat(payment): PI-853 BluesnapDirect added alphanumeric validation for the IBAN field

--- packages/bluesnap-direct-integration/src/validation-schemas/getSepaValidationSchema.ts
+++ packages/bluesnap-direct-integration/src/validation-schemas/getSepaValidationSchema.ts
@@ -4,7 +4,12 @@ import { object, ObjectSchema, string } from 'yup';
 
 export default memoize(function getSepaValidationSchema(language: LanguageService): ObjectSchema {
     return object({
-        iban: string().required(language.translate('payment.sepa_account_number_required')),
+        iban: string()
+            .matches(
+                /^[a-zA-Z0-9]+$/i,
+                language.translate(`address.invalid_characters_error`, { label: 'IBAN' }),
+            )
+            .required(language.translate('payment.sepa_account_number_required')),
         firstName: string().required(language.translate('address.first_name_required_error')),
         lastName: string().required(language.translate('address.last_name_required_error')),
     });
